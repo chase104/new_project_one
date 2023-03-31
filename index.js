ff18bed5a2db1a59af950fbfa1b29e765707e774
Chase Van Halen <cvanhalen@perscholas.org>
2023-03-31 13:55:43 -0500

    add some logs to file

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("this is my first change to the file")
+
+
+
+const logSomethingCool = () => {
+    console.log("something cool")
+}
+logSomethingCool();
+logSomethingCool();
+logSomethingCool();
